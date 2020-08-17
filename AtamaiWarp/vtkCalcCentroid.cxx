33fb18c47e191992ed0720e3d11a789b0d18fde5
VoxAtx <Voxatx@proton.me>
2020-08-17 17:31:40 +0000

    Bump version 1.0.2

---
+++ AtamaiWarp/vtkCalcCentroid.cxx
@@ -0,0 +1,19 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkCalcCentroid.cxx,v $
+  Language:  C++
+  Date:      $Date: 2007/08/24 20:02:25 $
+  Version:   $Revision: 1.8 $
+  Thanks:    Thanks to Yves who developed this class.
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyr
