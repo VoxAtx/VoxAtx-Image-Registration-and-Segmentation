f85deba1863e548d074e5093c352db5c4e125aeb
VoxAtx <Voxatx@proton.me>
2020-08-30 07:12:31 +0000

    batch push

--- AtamaiWarp/vtkCalcCrossCorrelation.cxx
+++ AtamaiWarp/vtkCalcCrossCorrelation.cxx
@@ -25,4 +25,34 @@ modification, are permitted provided that the following conditions are met:
    from this software without specific prior written permission.
 
  * Modified source versions must be plainly marked as such, and must not be
-   misrepresented 
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkCalcCrossCorrelation.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkCommand.h"
+#include "vtkImageData.h"
+#include "vtkImageStencilData.h"
+
+#if (VTK_MAJOR_VERSION >= 5) 
+#include "vtkInformation.h"
+#include "vtkExecutive.h"
+#endif
+
+//---------------------------------------------------------------------------
+vtkCalcCrossCorrelation* vtkCalcCrossCorrelation::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vt
