3844432e8e04a43adbccc089616f4e5a0b98e86e
VoxAtx <Voxatx@proton.me>
2020-09-20 23:11:31 +0000

    test theme

---
+++ AtamaiWarp/vtkImageDataStatistics.h
@@ -0,0 +1,27 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkImageDataStatistics.h,v $
+  Language:  C++
+  Date:      $Date: 2007/08/24 20:02:25 $
+  Version:   $Revision: 1.3 $
+  Thanks:    Thanks to Yves who developed this class.
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source 
