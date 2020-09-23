adc9dc6cb16c7d647ede85e900d7488dc7cdb6a0
VoxAtx <Voxatx@proton.me>
2020-09-23 21:06:15 +0000

    Update readme file

--- AtamaiWarp/vtkImageDataStatistics.h
+++ AtamaiWarp/vtkImageDataStatistics.h
@@ -24,4 +24,32 @@ modification, are permitted provided that the following conditions are met:
    of any contributors may be used to endorse or promote products derived
    from this software without specific prior written permission.
 
- * Modified source 
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkImageDataStatistics - Computes displacement grid statistics.
+//
+// .SECTION Description
+// Takes a 3 scalar component vtkImageData as input.
+// - computes mean and standard deviation of vector magnitude.
+
+#ifndef __vtkImageDataStatistics_h
+#define __vtkImageDataStatistics_h
+
+#include "vtkProcessObject.h"
+
+class vtkImageData;
+
+class VTK_EXPORT vtkImageDataSt
