ac1084bcc2658ea4d70d247a1b394e1e54de9acd
VoxAtx <Voxatx@proton.me>
2021-01-11 06:49:29 +0000

    First draft of README

--- ImageRegistration/vtkITKXFMWriter.cxx
+++ ImageRegistration/vtkITKXFMWriter.cxx
@@ -17,4 +17,40 @@ conditions are met:
 2) Redistribution in binary form must include the above copyright
    notice, a copy of this license and the following disclaimer
    in the documentation or with other materials provided with the
- 
+   distribution.
+
+3) Modified copies of the source code must be clearly marked as such,
+   and must not be misrepresented as verbatim copies of the source code.
+
+THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE SOFTWARE "AS IS"
+WITHOUT EXPRESSED OR IMPLIED WARRANTY INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE.  IN NO EVENT SHALL ANY COPYRIGHT HOLDER OR OTHER PARTY WHO MAY
+MODIFY AND/OR REDISTRIBUTE THE SOFTWARE UNDER THE TERMS OF THIS LICENSE
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, LOSS OF DATA OR DATA BECOMING INACCURATE
+OR LOSS OF PROFIT OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF
+THE USE OR INABILITY TO USE THE SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGES.
+
+=========================================================================*/
+
+#include "vtkITKXFMWriter.h"
+
+#include "vtkObjectFactory.h"
+
+#include "vtkMath.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+#include "vtkCollection.h"
+#include "vtkTransform.h"
+#include "vtkHomogeneousTransform.h"
+#include "vtkGeneralTransform.h"
+#include "vtkDoubleArray.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkPoints.h"
+
+#include <ctype.h>
+#include <stdio.h>
+
+#if !defined(_WIN32
