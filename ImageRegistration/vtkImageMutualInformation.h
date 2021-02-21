058e89032eb7787d70895a3f1057b271f113a8b9
VoxAtx <Voxatx@proton.me>
2021-02-21 16:05:35 +0000

    Create boobs.lua

--- ImageRegistration/vtkImageMutualInformation.h
+++ ImageRegistration/vtkImageMutualInformation.h
@@ -25,4 +25,38 @@
 // and the value to minimize to register the images can be retrieved.
 //
 // When using this metric, you must call SetInputRange() to set the range
-// for each of the input images.  The values will be c
+// for each of the input images.  The values will be clamped to the ranges
+// that you specify when the metric is computed.  If the images are likely
+// to have some voxel values that are outliers, then a winsorized range
+// should be used (that is, a range that excludes the outliers).
+//
+// References:
+//
+//  [1] D. Mattes, D.R. Haynor, H. Vesselle, T. Lewellen and W. Eubank,
+//      PET-CT Image Registration in the Chest Using Free-form Deformations,
+//      IEEE Transactions in Medical Imaging 22:120-128, 2003.
+//
+//  [2] C. Studholme, D.L.G. Hill and D.J. Hawkes,
+//      An Overlap Invariant Measure of 3D Medical Image Alignment,
+//      Pattern Recognition 32:71-86, 1999.
+
+#ifndef vtkImageMutualInformation_h
+#define vtkImageMutualInformation_h
+
+#include "vtkImageSimilarityMetric.h"
+
+class vtkImageMutualInformationTLS;
+
+class VTK_EXPORT vtkImageMutualInformation : public vtkImageSimilarityMetric
+{
+public:
+  static vtkImageMutualInformation *New();
+  vtkTypeMacro(vtkImageMutualInformation, vtkImageSimilarityMetric);
+
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Set the type for the output.  The joint histogram will always be
+  // computed using vtkIdType, but since vtkIdType is not directly
+  // supported as an image data type, it will be converted to the requested
+  // type for use as the output of the filter.  The default type is f
