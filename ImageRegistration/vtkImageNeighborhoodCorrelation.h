644797ce9bf0508c2310b1c043afb89fd83a764d
VoxAtx <Voxatx@proton.me>
2021-03-05 23:04:51 +0000

    ci

---
+++ ImageRegistration/vtkImageNeighborhoodCorrelation.h
@@ -0,0 +1,10 @@
+/*=========================================================================
+
+  Module: vtkImageNeighborhoodCorrelation.h
+
+  Copyright (c) 2016 David Gobbi
+  All rights reserved.
+  See Copyright.txt or http://dgobbi.github.io/bsd3.txt for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR
