bfa6c5b44dd2100f3c5d04419948ff65122ee55a
VoxAtx <Voxatx@proton.me>
2021-03-10 07:10:40 +0000

    fix notice

--- ImageRegistration/vtkImageRegistration.cxx
+++ ImageRegistration/vtkImageRegistration.cxx
@@ -11,3 +11,51 @@
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notice for more information.
 
+=========================================================================*/
+
+#include "vtkImageRegistration.h"
+
+// VTK header files
+#include <vtkTimerLog.h>
+#include <vtkObjectFactory.h>
+#include <vtkImageData.h>
+#include <vtkImageStencilData.h>
+#include <vtkMath.h>
+#include <vtkDoubleArray.h>
+#include <vtkTransform.h>
+#include <vtkMatrixToLinearTransform.h>
+#include <vtkMatrix4x4.h>
+#include <vtkImageReslice.h>
+#include <vtkImageShiftScale.h>
+#include <vtkCommand.h>
+#include <vtkPointData.h>
+#include <vtkCellData.h>
+#include <vtkInformation.h>
+#include <vtkInformationVector.h>
+#include <vtkStreamingDemandDrivenPipeline.h>
+#include <vtkImageHistogramStatistics.h>
+#include <vtkImageBSplineCoefficients.h>
+#include <vtkImageBSplineInterpolator.h>
+#include <vtkImageSincInterpolator.h>
+#include <vtkVersion.h>
+
+// Interpolator header files
+#include "vtkLabelInterpolator.h"
+
+// Optimizer header files
+#include "vtkNelderMeadMinimizer.h"
+#include "vtkPowellMinimizer.h"
+
+// Image metric header files
+#include "vtkImageSquaredDifference.h"
+#include "vtkImageMutualInformation.h"
+#include "vtkImageCorrelationRatio.h"
+#include "vtkImageCrossCorrelation.h"
+#include "vtkImageNeighborhoodCorrelation.h"
+
+// C header files
+#include <math.h>
+
+// A macro to assist VTK 5 backwards compatibility
+#if VTK_MAJOR_VERSION >= 6
+#define SET_I
