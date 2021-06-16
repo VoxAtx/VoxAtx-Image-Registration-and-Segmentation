18b51ea12c6be6cc23df1b98231196906b563b08
VoxAtx <Voxatx@proton.me>
2021-06-16 19:35:18 +0000

    minor update.

--- ImageSegmentation/vtkImageExtractPoints.h
+++ ImageSegmentation/vtkImageExtractPoints.h
@@ -21,4 +21,34 @@
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  A PARTICULAR P
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkImageExtractPoints - Extract all image voxels as points.
+// .SECTION Description
+// This filter takes an input image and an optional stencil, and creates
+// a vtkPolyData that contains the points and the point attributes but no
+// cells.  If a stencil is provided, only the points inside the stencil
+// are included.
+// .SECTION Thanks
+// Thanks to David Gobbi, Calgary Image Processing and Analysis Centre,
+// University of Calgary, for providing this class.
+
+#ifndef vtkImageExtractPoints_h
+#define vtkImageExtractPoints_h
+
+#include "vtkPolyDataAlgorithm.h"
+
+class vtkImageStencilData;
+
+class VTK_EXPORT vtkImageExtractPoints :
+  public vtkPolyDataAlgorithm
+{
+public:
