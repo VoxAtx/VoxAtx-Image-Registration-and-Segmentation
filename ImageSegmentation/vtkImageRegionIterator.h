e37c35cae103b6ad82b70b890739288ae961fce8
VoxAtx <Voxatx@proton.me>
2021-08-31 09:28:30 +0000

    add standard

---
+++ ImageSegmentation/vtkImageRegionIterator.h
@@ -0,0 +1,11 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkImageRegionIterator.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or F
