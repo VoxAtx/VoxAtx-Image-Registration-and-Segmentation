0fb227d99bc4df1c6dbe4a137a1527d03bdaf3a6
VoxAtx <Voxatx@proton.me>
2021-10-11 20:46:03 +0000

    - progress towards OODT-15 One trunk for all OODT components with top level build

--- Programs/skullstrip.cxx
+++ Programs/skullstrip.cxx
@@ -21,4 +21,58 @@ Module:    skullstrip.cxx
 #include <vtkImageData.h>
 #include <vtkPointData.h>
 #include <vtkMatrix4x4.h>
-#incl
+#include <vtkTransform.h>
+#include <vtkIntArray.h>
+#include <vtkStringArray.h>
+#include <vtkMath.h>
+#include <vtkCommand.h>
+
+#include <vtkMINCImageReader.h>
+#include <vtkMINCImageWriter.h>
+#include <vtkDICOMImageReader.h>
+
+#include <vtkRenderer.h>
+#include <vtkCamera.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleImage.h>
+#include <vtkImageSlice.h>
+#include <vtkImageStack.h>
+#include <vtkImageResliceMapper.h>
+#include <vtkImageProperty.h>
+#include <vtkWindowToImageFilter.h>
+#include <vtkPNGWriter.h>
+#include <vtkTIFFWriter.h>
+#include <vtkJPEGWriter.h>
+#include <vtkSTLWriter.h>
+#include <vtkMNIObjectWriter.h>
+#include <vtkPolyDataWriter.h>
+
+#include <vtkTimerLog.h>
+#include <vtkVersion.h>
+
+#include <vtksys/SystemTools.hxx>
+
+#include "AIRSConfig.h"
+#include "vtkImageMRIBrainExtractor.h"
+
+// optional readers
+#ifdef AIRS_USE_DICOM
+#define AIRS_USE_NIFTI
+#include <vtkNIFTIReader.h>
+#include <vtkNIFTIWriter.h>
+#include <vtkDICOMReader.h>
+#include <vtkDICOMSorter.h>
+#include <vtkDICOMMRGenerator.h>
+#include <vtkDICOMCTGenerator.h>
+#include <vtkDICOMWriter.h>
+#include <vtkDICOMMetaData.h>
+#include <vtkGlobFileNames.h>
+#endif
+
+#include <vector>
+#include <string>
+
+#include <stdlib.h>
+
+// A macro 
