41420f957e9d1c0067ea90212dc67bd7bff6ff8d
VoxAtx <Voxatx@proton.me>
2022-10-11 12:57:23 +0000

    why not

--- Testing/TestImageConnectivityFilter.cxx
+++ Testing/TestImageConnectivityFilter.cxx
@@ -21,4 +21,44 @@
 #include <vtkSmartPointer.h>
 #include <vtkCamera.h>
 #include <vtkImageData.h>
-#incl
+#include <vtkImageProperty.h>
+#include <vtkImageReader2.h>
+#include <vtkImageSlice.h>
+#include <vtkImageSliceMapper.h>
+#include <vtkInteractorStyleImage.h>
+#include <vtkIdTypeArray.h>
+#include <vtkIntArray.h>
+#include <vtkPoints.h>
+#include <vtkPointData.h>
+#include <vtkPolyData.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkVersion.h>
+
+#include <vtkTestUtilities.h>
+
+#include "AIRSConfig.h"
+#include "vtkImageConnectivityFilter.h"
+
+int main(int argc, char *argv[])
+{
+  vtkSmartPointer<vtkRenderWindowInteractor> iren =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  vtkSmartPointer<vtkInteractorStyleImage> style =
+    vtkSmartPointer<vtkInteractorStyleImage>::New();
+  style->SetInteractionModeToImageSlicing();
+  vtkSmartPointer<vtkRenderWindow> renWin =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  iren->SetRenderWindow(renWin);
+  iren->SetInteractorStyle(style);
+
+  char *fname =
+    vtkTestUtilities::ExpandDataFileName(argc, argv, "Data/headsq/quarter");
+
+  vtkSmartPointer<vtkImageReader2> reader =
+    vtkSmartPointer<vtkImageReader2>::New();
+  reader->SetDataByteOrderToLittleEndian();
+  reader->SetDataExtent(0, 63, 0, 63, 2, 4);
+  reader->SetDataSpacing(3.2, 3.2, 1.5);
+  reader->S
